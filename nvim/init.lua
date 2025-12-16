89fb64578e28892d96f8ba789de2dfaf30113095
d.stsepaniuk <odthmoon@gmail.com>
2025-12-16 07:27:46 +0100

    init: tmux, nvim nightly

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,19 @@
-require("core")
-require("config.lazy")
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.wrap = false
+vim.o.tabstop = 4
+vim.o.swapfile = false
+cim.o.clipboard:append("unnamedplus")
+
+vim.g.mapleader = " "
+
+vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
+vim.keymap.set('n', '<leader>w', ':write<CR>')
+vim.keymap.set('n', '<leader>q', ':quit<CR>')
+
+vim.pack.add({
+		{src = "https://github.com/vague2k/vague.nvim"},
+		{src = "https://github.com/stevearc/oil.nvim"},
+})
+
+vim.cmd("colorscheme vague")
